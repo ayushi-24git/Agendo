4ad0bd97babfe4959dd373a5f685001de7a22c0a
ayushi-24git <rastogee.ayushi@gmail.com>
2021-01-24 19:31:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.getElementById gets element with specified ID
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value; // whatever is entered
+    toDoContainer.appendChild(paragraph);
+    inputField.value = ""; // after adding, a fresh text is to be added
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
